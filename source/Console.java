8c82365f1d8f042c61c69ed844820ab4ca5be1fa
Terry Gu <gyoushijie@gmail.com>
2023-09-28 11:08:25 -0700

    Initial commit

---
+++ source/Console.java
@@ -0,0 +1,7 @@
+package source;
+
+public class Console {
+    public static void log (String s) {
+        System.out.printf("[INFO] %s\n", s);
+    }
+}
---
+++ source/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Filename: Main.java
+ * Description: Main entry of the game
+ * 
+ * @author Terry Gu (gyoushijie@gmail.com)
+ * last modified: 09/28/2023 10:53
+ */
+package source;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
